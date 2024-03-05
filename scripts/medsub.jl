0e1264e994c10eca9c6a4243f33a67b41e9a767b
William Thompson <wthompson@uvic.ca>
2024-03-05 10:44:52 -0800

    Rename avesub to medsub

--- scripts/medsub.jl
+++ scripts/medsub.jl
@@ -4,7 +4,7 @@ Given a single reference, typically and average stack of all frames,
 subtract it from the set of matching frames, rotate those residuals
 North-up, and then stack.
 """
-function avesub(pattern::AbstractString; force=false)
+function medsub(pattern::AbstractString; force=false)
     fnames = Glob.glob(pattern)
     @info "stacking master"
     stackframes(median, pattern; force)
@@ -13,7 +13,7 @@ function avesub(pattern::AbstractString; force=false)
     @info "subtracting"
     ref_img = collect(load(reference_img_fname))
     for fname in fnames
-        outname = replace(fname,".fits"=>".avesub.fits")
+        outname = replace(fname,".fits"=>".medsub.fits")
         if !force && isfile(outname) && Base.Filesystem.mtime(outname) > Base.Filesystem.mtime(fname)
             continue
         end
@@ -26,14 +26,14 @@ function avesub(pattern::AbstractString; force=false)
     end
 
     @info "stacking residuals (not North up)"
-    avesub_pattern = replace(pattern, ".fits"=>".avesub.fits")
-    stackframes(median, avesub_pattern; force)
+    medsub_pattern = replace(pattern, ".fits"=>".medsub.fits")
+    stackframes(median, medsub_pattern; force)
 
     @info "rotating residuals North up"
-    rotnorth(avesub_pattern; force)
+    rotnorth(medsub_pattern; force)
 
     @info "stacking north up residuals"
-    avesub_rotnorth_pattern = replace(pattern, ".fits"=>".avesub.rotnorth.fits")
-    stackframes(median, avesub_rotnorth_pattern; force)
-    # stackframes_contweight(median, avesub_rotnorth_pattern; force)
+    medsub_rotnorth_pattern = replace(pattern, ".fits"=>".medsub.rotnorth.fits")
+    stackframes(median, medsub_rotnorth_pattern; force)
+    # stackframes_contweight(median, medsub_rotnorth_pattern; force)
 end
